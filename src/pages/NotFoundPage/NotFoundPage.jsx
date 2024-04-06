54453372691fba8b9ed4c92267a902bd2582c374
sinhey <sinheyyy@gmail.com>
2024-04-06 23:54:52 +0900

    update not found page design

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './NotFoundPage.style.css';
 
 const NotFoundPage = () => {
-  return (
-    <div>
-      NotFoundPage
-    </div>
-  )
+    return (
+        <div className='main'>
+            <h1>이 페이지는 존재하지 않습니다🥹 url 확인부탁드립니다🐾</h1>
+            <img src="https://images.unsplash.com/photo-1425082661705-1834bfd09dca?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+        </div>
+    )
 }
 
 export default NotFoundPage
